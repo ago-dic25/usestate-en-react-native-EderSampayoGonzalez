6b13f7ec6cd3e47364cc8da8622592125b1a87e8
Eder Abraham Sampayo Gonzalez <151587047+EderSampayoGonzalez@users.noreply.github.com>
2025-10-26 22:51:58 -0600

    añadida app

--- App.js
+++ App.js
@@ -1,12 +1,63 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
-import { estiloTextos } from './misEstilos';
+import { StyleSheet, Text, View, TextInput, Button } from 'react-native';
+import { useState } from 'react';
 
 export default function App() {
+  const [contador, setContador] = useState(0);
+
+  const [nombre, setNombre] = useState('');
+  const [mensaje, setMensaje] = useState('');
+
+  function saludarAlumno() {
+    var emojis = contarCaracteres();
+    if (nombre.trim() === ''){
+        setMensaje(emojis + '\nEscribe tu nombre...');
+    } else {
+        setMensaje(emojis + `\nHola ${nombre}`);
+    }
+  }
+
+  function contarCaracteres() {
+    if (nombre.length === 0) {
+      return('🥀🥀🥀🥀🥀');
+    }
+    else if (nombre.length > 0 && nombre.length <= 10) {
+      return('💀💀💀💀💀');
+    }
+    else if (nombre.length > 10 && nombre.length <= 20) {
+      return('👻👻👻👻👻');
+    }
+    else {
+      return('👾👾👾👾👾');
+    }
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <Text style={estiloTextos.texto}>Hola, mundo</Text>
+      <Text>{mensaje}</Text>
+      <TextInput
+        placeholder="Escribe tu nombre"
+        value = {nombre}
+        onChangeText={(text) => setNombre(text)}
+        style={styles.input}
+      />
+
+      <Button title="Mostrar mensaje"
+        onPress={ saludarAlumno }
+        style={styles.boton}
+      />
+
+      <Button title="Limpiar Nombre de Alumno"
+        onPress={() => setNombre('')}
+        style={styles.boton}
+      />
+
+      <Button title="Aumentar"
+        onPress={() => setContador(contador + 1)}
+        style={styles.boton}
+      />
+      <Text>Haz hecho click {contador} veces.</Text>
+
       <StatusBar style="auto" />
     </View>
   );
@@ -15,8 +66,18 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#000',
+    backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
-  }
+  },
+
+  input: {
+    height: 40,
+    width: 200,
+    borderColor: 'gray',
+    borderWidth: 3,
+    borderRadius: 100,
+    padding: 10,
+    margin: 12,
+  },
 });
